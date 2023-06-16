6275376880c229627004a5c4cb0edd48c61c5a16
Lázaro José <40708051+LazaroJPR@users.noreply.github.com>
2023-06-16 14:11:40 -0300

    Realizar ação dos jogadores
    Adicionada função que realizam as ações dos jogadores de acordo com as regras do jogo.
    Também foi criado a função que realiza as rodadas

--- werewolf.pl
+++ werewolf.pl
@@ -10,44 +10,59 @@ grupos([curandeiro, vidente, aldeao, idiota, cacador, lobisomem]).
 :- dynamic jogadorGrupo/2.
 
 % --> Açõess
+acao(nada, aldeao).
 acao(proteger, curandeiro).
 acao(verificar, vidente).
 acao(matar, lobisomem).
 acao(escolherOutro, idiota).
 acao(eliminar, cacador).
 
-% --> Turnos
-turno(dia).
-turno(noite).
-
-% --> Votação
-votacao(iniciar).
-votacao(finalizada).
-
-% --> Status do jogador
+% --> Status
 status(vivo).
 status(morto).
 status(protegido).
 
+% --> Status do jogador
+jogadorStatus(jogador01, vivo).
+jogadorStatus(jogador02, vivo).
+jogadorStatus(jogador03, vivo).
+jogadorStatus(jogador04, vivo).
+jogadorStatus(jogador05, vivo).
+jogadorStatus(jogador06, vivo).
+jogadorStatus(jogador07, vivo).
+jogadorStatus(jogador08, vivo).
+jogadorStatus(jogador09, vivo).
+jogadorStatus(jogador10, vivo).
+jogadorStatus(jogador11, vivo).
+jogadorStatus(jogador12, vivo).
+jogadorStatus(jogador13, vivo).
+jogadorStatus(jogador14, vivo).
+jogadorStatus(jogador15, vivo).
+jogadorStatus(jogador16, vivo).
+jogadorStatus(jogador17, vivo).
+jogadorStatus(jogador18, vivo).
+jogadorStatus(jogador19, vivo).
+jogadorStatus(jogador20, vivo).
+
 % FUNÇÕES
 % -------------------------------------------------------------------------
 % --> Gera a relação entre jogador e grupo aleatoriamente
-% 50% de chance de ser aldeão
-% Pelo menos um lobosiomem é relacionado
 gerarRelacao :-
     retractall(jogadorGrupo(_, _)), % Limpa qualquer relação anterior
     jogadores(Jogadores),
     grupos(Grupos),
-    random_member(Lobisomem, Grupos), % Seleciona aleatoriamente um lobisomem
-    random_permutation(Jogadores, JogadoresEmbaralhados), % Embaralha a lista de jogadores
+    random_member(Lobisomem, Grupos),
+    random_permutation(Jogadores, JogadoresEmbaralhados),
     inserirGrupo(Lobisomem, JogadoresEmbaralhados, Grupos).
 
+% --> Relaciona os grupos com jogadores
+% 50% de chance de ser aldeão e pelo menos um lobosiomem é relacionado
 inserirGrupo(_, [], _).
 inserirGrupo(Lobisomem, [Jogador|Resto], Grupos) :-
-    random(0, 2, Chance), % Gera um número aleatório entre 0 e 1
-    (Chance =:= 0 -> Grupo = aldeao ; random_member(Grupo, Grupos)), % 50% de chance de ser aldeão, caso contrário seleciona aleatoriamente um grupo
-    assertz(jogadorGrupo(Jogador, Grupo)), % Insere a relação jogadorGrupo
-    (Grupo = Lobisomem -> % Verifica se o grupo é lobisomem
+    random(0, 2, Chance), 
+    (Chance =:= 0 -> Grupo = aldeao ; random_member(Grupo, Grupos)),
+    assertz(jogadorGrupo(Jogador, Grupo)),
+    (Grupo = Lobisomem ->
         delete(Grupos, Lobisomem, NovosGrupos),
         inserirGrupo(Lobisomem, Resto, NovosGrupos)
     ; inserirGrupo(Lobisomem, Resto, Grupos)
@@ -65,6 +80,102 @@ imprimirRelacoes([Jogador-Grupo|Resto]) :-
     write(Jogador), write(' - '), write(Grupo), nl,
     imprimirRelacoes(Resto).
 
+% --> Imprime os jogadores e seus status
+imprimirJogadoresStatus :-
+    jogadorStatus(Jogador, Status),
+    write('Jogador: '), write(Jogador), write(' - Status: '), write(Status), nl,
+    fail.
+
+%  --> atualiza o status do jogador
+atualizarStatusJogador(Jogador, NovoStatus) :-
+    retract(jogadorStatus(Jogador, _)),
+    assertz(jogadorStatus(Jogador, NovoStatus)).
+
+%  --> atualiza o grupo do jogador
+atualizarGrupoJogador(Jogador, NovoGrupo) :-
+    retract(jogadorGrupo(Jogador, _)),
+    assertz(jogadorGrupo(Jogador, NovoGrupo)).
+
+%  --> executa a ação do jogador e chama recursivamente para os próximos jogadores
+todosJogadoresAcaoRealizada([]).
+todosJogadoresAcaoRealizada([Jogador | OutrosJogadores]) :-
+    jogadorStatus(Jogador, vivo),
+    jogadorGrupo(Jogador, Grupo),
+    acao(Acao, Grupo),
+    random_permutation(OutrosJogadores, JogadoresEmbaralhados),
+    select(JogadorAleatorio, JogadoresAleatorios, NovosOutrosJogadores),
+    executarAcao(Jogador, Acao, JogadorAleatorio),
+    todosJogadoresAcaoRealizada([JogadorAleatorio | NovosOutrosJogadores]).
+
+%  --> Altera o status de todos os jogadores protegidos para vivos
+retirarProtecao([]).
+retirarProtecao([Jogador | OutrosJogadores]) :-
+    atualizarStatusJogador(JogadorEscolhido, vivo),
+    retirarProtecao(OutrosJogadores).
+
+%  --> Acao específica de quando um cacador morre
+acaoCacador(Jogador):-
+    	jogadorGrupo(Jogador, cacador),
+        findall(Jogador, jogadorStatus(Jogador, vivo), JogadoresVivos),
+        random_permutation(JogadoresVivos, [JogadorEscolhido | OutrosJogadores]),
+        atualizarStatusJogador(JogadorEscolhido, morto),
+        write('Jogador '), write(Jogador), write(' era um cacador e morreu levando o jogador '),  write(JogadorEscolhido), write(' junto.'), nl. 
+
+%  --> Acao específica de quando um idiota morre
+acaoIdiota(Jogador) :-
+    jogador(Jogador, idiota),
+    findall(Jogador, jogadorStatus(Jogador, vivo), JogadoresVivos),
+    random_permutation(JogadoresVivos, [JogadorEscolhido | OutrosJogadores]),
+    atualizarGrupoJogador(JogadorEscolhido, idiota),
+    write('Jogador '), write(Jogador), write(' era um idiota e morreu mudando o grupo de outro jogador para idiota'), nl. 
+
+% --> Regras para executar uma ação específica para cada jogador
+executarAcao(Jogador, nada, JogadorAleatorio) :-
+    write('Jogador '), write(Jogador), write(' realizou sua acao.'), nl.
+
+executarAcao(Jogador, proteger, JogadorAleatorio) :-
+    atualizarStatusJogador(JogadorAleatorio, protegido),
+    write('Jogador '), write(Jogador), write(' realizou sua acao.'), nl.    
+
+executarAcao(Jogador, verificar, JogadorAleatorio) :-
+    write('Jogador '), write(Jogador), write(' verificou o jogador '), write(JogadorAleatorio), write(' e ele é um '), write(jogadorGrupo(JogadorAleatorio, X)), nl.
+
+executarAcao(Jogador, matar, JogadorAleatorio) :-
+    jogadorStatus(JogadorAleatorio, vivo),  % Verificacao para saber se o jogador não está morto nem está protegido
+    atualizarStatusJogador(JogadorAleatorio, morto),
+    acaoCacador(JogadorAleatorio),
+    acaoIdiota(JogadorAleatorio,)
+    write('Jogador '), write(Jogador), write(' realizou sua acao.'), nl.
+
+executarAcao(Jogador, escolherOutro, JogadorAleatorio) :-
+    write('Jogador '), write(Jogador), write(' realizou sua acao.'), nl.
+
+executarAcao(Jogador, eliminar, JogadorAleatorio) :-
+    write('Jogador '), write(Jogador), write(' realizou sua acao.'), nl.
+
+% --> Chama a execução das ações de todos os jogadores
+realizarAoes():-
+    findall(Jogador, jogadorStatus(Jogador, vivo), JogadoresVivos),
+    todosJogadoresAcaoRealizada(JogadoresVivos),
+    findall(Jogador, jogadorStatus(Jogador, protegido), JogadoresProtegidos),
+    retirarProtecao(JogadoresProtegidos).
+
+% INTERFACE
+% -------------------------------------------------------------------------
+% --> Realiza cada rodada
+rodadas:-
+    writeln('========================================================================================'),
+    lua,
+    write('A noite chegou, os aldeões foram para suas casas, o que poderá acontecer nesta noite? '), nl,
+    realizarAcoes,
+    writeln('========================================================================================'),
+    sol,
+    write('A noite passou, vamos ver o que aconteceu? '), nl,
+    imprimirJogadoresStatus,
+    write('Com esse caos que acoteceu esta noite, precisamos fazer uma votação para eliminar o nosso problema, o lobisomem! '), nl,
+    %FAZER PARTE DA VOTACAO
+    %FAZER PARTE DA FINALIZACAO DO JOGO
+
 % Exibir o menu
 exibir_menu :-
     writeln('========================================================================================'),
@@ -72,8 +183,6 @@ exibir_menu :-
     write('[ 2 ] SAIR'), nl, nl,
     write('Digite sua opcao: '), nl.
 
-% INTERFACE
-% -------------------------------------------------------------------------
 % Menu
 menu :-
     repeat,
@@ -90,13 +199,48 @@ menu :-
     nl,
     fail.
 
+lua :-
+    writeln('o                     __...__     *                '),
+    writeln('              *   .--     __.=-.             o     '),
+    writeln('     |          ./     .-                          '),
+    writeln('    -O-        /      /                            '),
+    writeln('     |        /     "/               *             '),
+    writeln('             |     (@)                             '),
+    writeln('            |        \                         .   '),
+    writeln('            |         \                            '),
+    writeln(' *          |       ___\                  |        '),
+    writeln('             |  .   /  `                 -O-       '),
+    writeln('              \  `~~\                     |        '),
+    writeln('         o     \     \            *                '),
+    writeln('                `\    `-.__           .            '),
+    writeln('    .             `--._    `--                     '),
+    writeln('                       `---~~`                *    '),
+    writeln('            *                   o                  '),
+
+sol :-
+    writeln('        \     (      /         '),
+    writeln('   `.    \     )    /    .     '),
+    writeln('     `.   \   (    /   .       '),
+    writeln('       `.  .-''''-.  .         '),
+    writeln(' `~._    . /_    _\`.    _.~   '),
+    writeln('     `~ /  / \  / \  \ ~       '),
+    writeln('_ _ _ _|  _\O/  \O/_  |_ _ _ _ '),
+    writeln('       | (_)  /\  (_) |        '),
+    writeln('    _.~ \  \      /  / ~._     '),
+    writeln(' .~      `. `.__. .      `~.   '),
+    writeln('       .'  `-,,,,-'  `.        '),
+    writeln('     .    /    )   \   `.      '),
+    writeln('   .     /    (     \    `.    '),
+    writeln('        /      )     \         '),
+    writeln('              (                '),
+
 iniciar :-
-        writeln(' __      ____________________________________      __________  .____   ___________'),
-    writeln('/  \\    /  \\_   _____/\\______   \\_   _____/  \\    /  \\_____  \\ |    |  \\_   _____/'),
-    writeln('\\   \\/\\/   /|    __)_  |       _/|    __)_\\   \\/\\/   //   |   \\|    |   |    __)  '),
-    writeln(' \\        / |        \\ |    |   \\|        \\        //    |    \\    |___|     \\   '),
-    writeln('  \\__/\\  / /_______  / |____|_  /_______  / \\__/\\  / \\_______  /_______ \\___  /   '),
-    writeln('       \\/          \\/         \\/        \\/       \\/          \\/        \\/   \\  /    '),
+        writeln(' __      ____________________________________      __________  .____   ___________         '),
+    writeln('/  \\    /  \\_   _____/\\______   \\_   _____/  \\    /  \\_____  \\ |    |  \\_   _____/     '),
+    writeln('\\   \\/\\/   /|    __)_  |       _/|    __)_\\   \\/\\/   //   |   \\|    |   |    __)        '),
+    writeln(' \\        / |        \\ |    |   \\|        \\        //    |    \\    |___|     \\           '),
+    writeln('  \\__/\\  / /_______  / |____|_  /_______  / \\__/\\  / \\_______  /_______ \\___  /          '),
+    writeln('       \\/          \\/         \\/        \\/       \\/          \\/        \\/   \\  /       '),
     writeln('          .                                                  .                  '),
     writeln('               ( ,(/                                       .((. (               '),
     writeln('               ,*      /(*    ./((/.         *((/.    /(*      (                '),
