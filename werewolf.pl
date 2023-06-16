180d07356644a0c48ce53632478de3f43f5d6614
Lázaro José Pedrosa dos Reis <lazaro.pedrosa@outlook.com>
2023-06-15 21:44:50 -0300

    Create werewolf.pl

---
+++ werewolf.pl
@@ -0,0 +1,38 @@
+grupo(lobisomem).
+grupo(curandeiro).
+grupo(vidente).
+grupo(aldeao).
+grupo(idiota).
+
+
+menuInicial :-
+    writeln(' __      ____________________________________      __________  .____     _____ '),
+    writeln('/  \    /  \_   _____/\______   \_   _____/  \    /  \_____  \ |    |  _/ ____\'),
+    writeln('\   \/\/   /|    __)_  |       _/|    __)_\   \/\/   //   |   \|    |  \   __\ '),
+    writeln(' \        / |        \ |    |   \|        \\        //    |    \    |___|  |   '),
+    writeln('  \__/\  / /_______  / |____|_  /_______  / \__/\  / \_______  /_______ \__|   '),
+    writeln('       \/          \/         \/        \/       \/          \/        \/      '),
+    writeln('          .                                                  .                  '),
+    writeln('               ( ,(/                                       .((. (               '),
+    writeln('               ,*      /(*    ./((/.         *((/.    /(*      (                '),
+    writeln('                (  ,*((.                                .((/. ,*                '),
+    writeln('                 (  (            (/(.((*(/(.((*            /  (                 '),
+    writeln('                 ,*  (                                    (  (                  '),
+    writeln('                  (                                         ,*                  '),
+    writeln('                   (                ,((.*(/                 (                   '),
+    writeln('                   (             /((.      ((/              (                   '),
+    writeln('                  /.        *((.                *((,        *,                  '),
+    writeln('                  (         ,*  ( ,(((.  *(((..(  (          (                  '),
+    writeln('                 ,* ,(.       (.    //    (,    *(       *(  (                  '),
+    writeln('                 (      /(                           ,(,      /                 '),
+    writeln('                 *(.      (   (        /.        (  .*      ,(,                 '),
+    writeln('                     ((       *,       /.       /        (*                     '),
+    writeln('                         ((    (       /.       (    (/                         '),
+    writeln('                             (/(       /.      ,,(/                             '),
+    writeln('                                /      /.      (                                '),
+    writeln('                                (      /      ./                                '),
+    writeln('                                ,*  *,    (   (                                 '),
+    writeln('                                 //////////////                                 '),
+
+iniciar :-
+    menuInicial.
