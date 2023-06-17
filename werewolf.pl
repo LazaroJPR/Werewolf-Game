c26dbefb2932164d70935cc78e36541d53991edc
Lázaro José <40708051+LazaroJPR@users.noreply.github.com>
2023-06-17 16:12:31 -0300

    Finalização
    Implementação do sistema de votação
    Implementação do sistema que finaliza o jogo
    Correção de bugs

--- werewolf.pl
+++ werewolf.pl
@@ -12,6 +12,8 @@ grupos([curandeiro, vidente, aldeao, idiota, cacador, lobisomem]).
 % --> Predicativo dinâmico do jogadorStatus
 :- dynamic jogadorStatus/2.
 
+:- dynamic voto/2.
+
 % --> Açõess
 acao(nada, aldeao).
 acao(proteger, curandeiro).
@@ -61,12 +63,17 @@ imprimirRelacoes([Jogador-Grupo|Resto]) :-
     write(Jogador), write(' - '), write(Grupo), nl,
     imprimirRelacoes(Resto).
 
-% --> Imprime os jogadores e seus status
-imprimirJogadoresStatus.
+% --> Ordena os jogadores em ordem "Numérica"
 imprimirJogadoresStatus :-
-    jogadorStatus(Jogador, Status),
-    write('Jogador: '), write(Jogador), write(' - Status: '), write(Status), nl,
-    fail.
+    findall(Jogador-Status, jogadorStatus(Jogador, Status), Relacoes),
+    sort(Relacoes, RelacoesOrdenadas),
+    imprimirRelacoes(RelacoesOrdenadas).
+
+% --> Imprime os jogadores e seus status
+imprimirStatus([]).
+imprimirStatus([Jogador-Status|Resto]) :-
+    write(Jogador), write(' - '), write(Status), nl,
+    imprimirRelacoes(Resto).
 
 % --> Coloca todos os status dos jogadores como vivo
 atualizarStatusVivo :-
@@ -145,10 +152,11 @@ executarAcao(Jogador, verificar, JogadorAleatorio) :-
 % --> Ação específica do lobisomem
 executarAcao(Jogador, matar, JogadorAleatorio) :-
     jogadorStatus(JogadorAleatorio, vivo),  % Verificacao para saber se o jogador não está morto nem está protegido
+    ((jogadorGrupo(JogadorAleatorio, lobisomem), write('Jogador '), write(Jogador), write(' realizou sua acao.'), nl); % Lobisomem não mata lobisomem
     atualizarStatusJogador(JogadorAleatorio, morto),
     acaoCacador(JogadorAleatorio),
     acaoIdiota(JogadorAleatorio),
-    write('Jogador '), write(Jogador), write(' realizou sua acao.'), nl.
+    write('Jogador '), write(Jogador), write(' realizou sua acao.'), nl).
 
 % --> Ação específica do idiota
 executarAcao(Jogador, escolherOutro, _) :-
@@ -165,23 +173,95 @@ realizarAcoes():-
     findall(Jogador, jogadorStatus(Jogador, protegido), JogadoresProtegidos),
     retirarProtecao(JogadoresProtegidos).
 
+% --> Gera votos aleatoriamente entre os jogadores
+gerarVotosAleatorios([], _ ).
+gerarVotosAleatorios([Eleitor|Resto], Votados) :-
+    random_permutation(Votados, [Votado| _]),
+    assertz(voto(Eleitor, Votado)),
+    write(Eleitor), write(' votou.'), nl,
+    gerarVotosAleatorios(Resto, Votados).
+
+% --> Conta os votos de todos os jogadores
+contarVotos(Jogador, Contagem) :-
+    findall(Jogador, voto(_, Jogador), Votos),
+    length(Votos, Contagem).
+
+% --> Verifica qual jogador foi o mais votado
+jogadorMaisVotado(JogadorMaisVotado) :-
+    jogadores(Jogadores),
+    maplist(contarVotos, Jogadores, Votos),
+    max_list(Votos, MaxVotos),
+    nth1(Indice, Votos, MaxVotos),
+    nth1(Indice, Jogadores, JogadorMaisVotado).
+
+% --> Verifica se existe lobisomem vivo
+naoExisteLobisomem([]).
+naoExisteLobisomem([Jogador|Resto]) :-
+    \+ jogadorGrupo(Jogador, lobisomem),
+    naoExisteLobisomem(Resto).
+
+% --> Verifica se existe jogador que nao seja lobisomem e que esteja vivo
+naoExisteJogadorGrupo([], _).
+naoExisteJogadorGrupo([Jogador|Resto], Grupo) :-
+    \+ jogadorGrupo(Jogador, Grupo),
+    naoExisteJogadorGrupo(Resto, Grupo).
+
+% --> Realiza verificacoes para chegar ao fim do jogo
+verificaFimDeJogo :-
+    findall(Jogador, jogadorStatus(Jogador, vivo), JogadoresVivos),
+    (
+        (naoExisteLobisomem(JogadoresVivos), vencer);
+        (naoExisteJogadorGrupo(JogadoresVivos, aldeao), 
+        naoExisteJogadorGrupo(JogadoresVivos, vidente), 
+        naoExisteJogadorGrupo(JogadoresVivos, curandeiro), 
+        naoExisteJogadorGrupo(JogadoresVivos, idiota), 
+        naoExisteJogadorGrupo(JogadoresVivos, cacador), perder)
+    ).
+
+% --> Iniciar a votacao
+votacaoIniciar :-
+    findall(Jogador, jogadorStatus(Jogador, vivo), JogadoresVivos),
+    gerarVotosAleatorios(JogadoresVivos, JogadoresVivos),
+    jogadorMaisVotado(JogadorVotado),
+    atualizarStatusJogador(JogadorVotado, morto),
+    writeln('========================================================================================'),
+    nl, write('Votacao encerrada, o jogador mais votado foi o '), write(JogadorVotado), write(' e ele foi eliminado!'), nl,nl.
+
 % INTERFACE
 % -------------------------------------------------------------------------
 % --> Realiza cada rodada
 rodadas :-
+    \+ verificaFimDeJogo,
     writeln('========================================================================================'),
+    noite,
     lua,
-    write('A noite chegou, os aldeoes foram para suas casas, o que podera acontecer nesta noite? '), nl, nl,
+    writeln('========================================================================================'),
+    nl, writeln('A noite chegou, os aldeoes foram para suas casas, o que podera acontecer nesta noite? '), nl,
+    write('[ 1 ] PASSAR A NOITE'), nl,
+    read(_),
+    writeln('========================================================================================'),
     realizarAcoes,
     writeln('========================================================================================'),
+    dia,
     sol,
-    write('A noite passou, vamos ver o que aconteceu? '), nl,
+    writeln('========================================================================================'),
+    nl, writeln('A noite passou, vamos ver o que aconteceu? '), nl,
+    write('[ 1 ] MOSTRAR O QUE ACONCECEU'), nl,
+    read(_),
+    writeln('========================================================================================'),
     imprimirJogadoresStatus,
-    write('Com esse caos que acoteceu esta noite, precisamos fazer uma votacao para eliminar o nosso problema, o lobisomem! '), nl,
-    %FAZER PARTE DA VOTACAO
-    %FAZER PARTE DA FINALIZACAO DO JOGO
-    %SOL É SÓ PRA FECHAR A FUNCAO
-    sol.
+    writeln('========================================================================================'),
+    \+ verificaFimDeJogo,
+    nl, writeln('Com esse caos que acoteceu esta noite, precisamos fazer uma votacao para eliminar o nosso problema, o lobisomem! '), nl,
+    write('[ 1 ] REALIZAR VOTACAO'), nl,
+    read(_),
+    writeln('========================================================================================'),
+    \+ verificaFimDeJogo,
+    votacao,
+    votacaoIniciar,
+    write('[ 1 ] INICIAR NOITE'), nl,
+    read(_),
+    rodadas.
 
 % Exibir o menu
 exibir_menu :-
@@ -191,15 +271,14 @@ exibir_menu :-
     write('Digite sua opcao: '), nl.
 
 % Menu
-menu :-
-    %repeat,
+iniciar :-
+    logo,
+    lobo,
     exibir_menu,
     read(Opcao),
     (Opcao = 1 ->(
             gerarRelacao,
             atualizarStatusVivo,
-            %imprimirJogadoresStatus,
-            imprimirJogadores,
             rodadas
         );
      Opcao = 2 -> halt;
@@ -208,6 +287,32 @@ menu :-
     Opcao \= 2,
     nl,
     fail.
+
+vencer :-
+    vencemos,
+    aldeao,
+    writeln('========================================================================================'),
+    writeln('Os aldeoes conseguiram eliminar todos os lobisomens da vila!!'), nl,
+    imprimirJogadores,
+    nl, writeln('[ 2 ] SAIR'), nl, nl,
+    read(Opcao),
+    (
+     Opcao = 2 -> halt;
+     halt
+    ).
+
+perder :-
+    perdemos,
+    lobo,
+    writeln('========================================================================================'),
+    writeln('Os lobisomens conseguiram matar todos os habitantes da vila!!'), nl,
+    imprimirJogadores,
+    nl, writeln('[ 2 ] SAIR'), nl, nl,
+    read(Opcao),
+    (
+     Opcao = 2 -> halt;
+     halt
+    ).
 
 lua :-
     writeln('o                     __...__     *                    '),
@@ -244,13 +349,55 @@ sol :-
     writeln('        /      )     \\            '),
     writeln('              (                    ').
 
-iniciar :-
+dia :-
+writeln('   ________  .___   _____   '),
+writeln('   \\______ \\ |   | /  _  \\  '),
+writeln('    |    |  \\|   |/  /_\\  \\ '),
+writeln('    |    `   \\   /    |    \\'),
+writeln('   /_______  /___\\____|__  /'),
+writeln('           \\/            \\/ ').
+
+noite :-
+    writeln(' _______   ________  ._________________________        '),
+    writeln(' \\      \\  \\_____  \\ |   \\__    ___/\\_   _____/  '),
+    writeln(' /   |   \\  /   |   \\|   | |    |    |    __)_       '),
+    writeln('/    |    \\/    |    \\   | |    |    |        \\     '),
+    writeln('\\____|__  /\\_______  /___| |____|   /_______  /      '),
+    writeln('        \\/         \\/                       \\/      ').
+
+votacao :-
+    writeln('____   ____________________________  _________     _____   ________                '),
+    writeln('\\   \\ /   /\\_____  \\__    ___/  _  \\ \\_   ___ \\   /  _  \\  \\_____  \\     '),
+    writeln(' \\   Y   /  /   |   \\|    | /  /_\\  \\/    \\  \\/  /  /_\\  \\  /   |   \\     '),
+    writeln('  \\     /  /    |    \\    |/    |    \\     \\____/    |    \\/    |    \\       '),
+    writeln('   \\___/   \\_______  /____|\\____|__  /\\______  /\\____|__  /\\_______  /       '),
+    writeln('                   \\/              \\/        \\/         \\/         \\/         ').
+
+logo :-
     writeln(' __      ____________________________________      __________  .____   ___________             '),
     writeln('/  \\    /  \\_   _____/\\______   \\_   _____/  \\    /  \\_____  \\ |    |  \\_   _____/     '),
     writeln('\\   \\/\\/   /|    __)_  |       _/|    __)_\\   \\/\\/   //   |   \\|    |   |    __)        '),
     writeln(' \\        / |        \\ |    |   \\|        \\        //    |    \\    |___|     \\           '),
     writeln('  \\__/\\  / /_______  / |____|_  /_______  / \\__/\\  / \\_______  /_______ \\___  /          '),
-    writeln('       \\/          \\/         \\/        \\/       \\/          \\/        \\/   \\  /       '),
+    writeln('       \\/          \\/         \\/        \\/       \\/          \\/        \\/   \\  /       ').
+
+perdemos :-
+    writeln('_______________________________________  ___________   _____   ________    _________._.            '),
+    writeln('\\______   \\_   _____/\\______   \\______ \\ \\_   _____/  /     \\  \\_____  \\  /   _____/| |   '),
+    writeln(' |     ___/|    __)_  |       _/|    |  \\ |    __)_  /  \\ /  \\  /   |   \\ \\_____  \\ | |      '),
+    writeln(' |    |    |        \\ |    |   \\|    `   \\|        \\/    Y    \\/    |    \\/        \\ \\|    '),
+    writeln(' |____|   /_______  / |____|_  /_______  /_______  /\\____|__  /\\_______  /_______  / __          '),
+    writeln('                  \\/         \\/        \\/        \\/         \\/         \\/        \\/  \\/    ').
+
+vencemos :-
+    writeln('____   _______________ _______  _________ ___________   _____   ________    _________._.           '),
+    writeln('\\   \\ /   /\\_   _____/ \\      \\ \\_   ___ \\\\_   _____/  /     \\  \\_____  \\  /   _____/| |'),
+    writeln(' \\   Y   /  |    __)_  /   |   \\/    \\  \\/ |    __)_  /  \\ /  \\  /   |   \\ \\_____  \\ | |  '),
+    writeln('  \\     /   |        \\/    |    \\     \\____|        \\/    Y    \\/    |    \\/        \\ \\|  '),
+    writeln('   \\___/   /_______  /\\____|__  /\\______  /_______  /\\____|__  /\\_______  /_______  / __      '),
+    writeln('                   \\/         \\/        \\/        \\/         \\/         \\/        \\/  \\/   ').
+
+lobo :-
     writeln('          .                                                  .                  '),
     writeln('               ( ,(/                                       .((. (               '),
     writeln('               ,*      /(*    ./((/.         *((/.    /(*      (                '),
@@ -271,5 +418,32 @@ iniciar :-
     writeln('                                /      /.      (                                '),
     writeln('                                (      /      ./                                '),
     writeln('                                ,*  *,    (   (                                 '),
-    writeln('                                 //////////////                                 '),
-    menu.
+    writeln('                                 //////////////                                 ').
+
+aldeao :-
+    writeln('                                                                                '),
+    writeln('                               ************                                     '),
+    writeln('                            .*     .**      *                                   '),
+    writeln('                            *               .,                                  '),
+    writeln('                            *                *                                  '),
+    writeln('                           .,                *                                  '),
+    writeln('                   ,****,, *                 ,..,,****.                         '),
+    writeln('                   .*****,.                   .,,*****                          '),
+    writeln('                          * *  /*.      .*   *.*                                '),
+    writeln('                           **    ,      *    **   ,.   *    *    ,   ,,         '),
+    writeln('                            *      *  *      *    *.   *    *   .*   ,,         '),
+    writeln('           .** .,           *      ***.      *    *.   *    *   .*   ,,         '),
+    writeln('           * *              *     ,,,,,     *     *.   *    *   .*   ,,         '),
+    writeln('         *  **..,            ,*           *,      ,.   *    *   .*   ,.         '),
+    writeln('         *      *              /**,   .***.        *   *    *   .*   *          '),
+    writeln('          ,****            **   *       .*  .*      ,* *    *   .* *.           '),
+    writeln('           *  *    *         *   .*    *    *         ***,  *  ,*,              '),
+    writeln('           *  *   *           *  ,**  */   *           *    *                   '),
+    writeln('         *********             **   *.  ***            **/******,               '),
+    writeln('         * .******                  *.                  ****** .,               '),
+    writeln('         *  ....,*                  *.                  *..... .,               '),
+    writeln('         *       *    **            *.            **    *      .,               '),
+    writeln('         * * *  *    ,**            *.            **     *  *  .,               '),
+    writeln('         * * *  *    * *            *.            * *    *  *  .,               '),
+    writeln('         * ,.*  *   *  *            *.            * ,,   *  *  .,               '),
+    writeln('         * ,.*  *  .*  *            *.            *  *   *  *  .,               ').
